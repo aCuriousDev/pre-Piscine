9dcc7d5d6a816fdce64574ffc474071d381165f6
aCuriousDev <quentinmarius@gmail.com>
2023-09-20 10:59:13 +0200

    updated so it can display all 4 figures

--- Day08/matplotlib and numpy/04.py
+++ Day08/matplotlib and numpy/04.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 import math
 
 
-def plot_fct(func, start, end):
+def plot_fct(func, start, end, title):
     # Generate x values
     x = np.linspace(start, end, 400)
 
     # Compute y values using the provided function
     y = [func(i) for i in x]
 
+    # Create a new figure window
+    plt.figure()
+
     # Plot the function
     plt.plot(x, y)
 
     # Add title, labels, and grid
-    plt.title('Function Plot')
+    plt.title(title)
     plt.xlabel('X values')
     plt.ylabel('Y values')
     plt.grid(True)
-
-    # Display the plot
-    plt.show()
 
 # Example usage
 
@@ -29,7 +29,10 @@ def f(x):
     return x**2 + x*3 + 2
 
 
-plot_fct(math.sin, 0, 50)
-plot_fct(f, -100, 200)
-plot_fct(lambda x: x**2, -10, 10)
-plot_fct(lambda x: 1/x, -100, 100)
+plot_fct(math.sin, 0, 50, 'sin(x)')
+plot_fct(f, -100, 200, 'x^2 + 3x + 2')
+plot_fct(lambda x: x**2, -10, 10, 'x^2')
+plot_fct(lambda x: 1/x, -100, 100, '1/x')
+
+# Display all the plots
+plt.show()
